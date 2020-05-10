6b19e0fbe109c382623c27023fb177a101d04fab
Liam Morrison <liammorrison03@gmail.com>
2020-05-10 12:11:43 -0500

    Blockie is now unable to escape the canvas.

--- src/blockie.js
+++ src/blockie.js
@@ -1,15 +1,17 @@
 const canvas = document.getElementById('game');
 const context = canvas.getContext('2d');
 
-let KeysPressed = {};
 let blockieSpeed = 2;
+let KeysPressed = {};
+let xInput = 0;
+let yInput = 0;
 
 class Player {
     constructor() {
         this.x = 64;
         this.y = 64;
-        this.dx = 0;
-        this.dy = 0;
+        this.testXLocation = this.x;
+        this.testYLocation = this.y;
         this.sideLength = 16;
     };
 };
@@ -20,58 +22,60 @@ function calculateAngleRadians(x, y) {
     return Math.atan2(y, x);
 }
 
+//Adds all currently pressed keys as a key with a pair of true in the KeysPressed object.
+document.addEventListener('keydown', e => {
+    KeysPressed[e.key] = true;
+});
+
+//Deletes all currently unpressed keys from the KeysPressed object.
+document.addEventListener('keyup', e => {
+    delete KeysPressed[e.key];
+});
+
 function loop() {
     //Clears the canvas so that it can later be redrawn with updated locations.
     context.clearRect(0, 0, canvas.width, canvas.height);
 
-    //Adds all currently pressed keys as a key with a pair of true in the KeysPressed object.
-    document.addEventListener('keydown', e => {
-        KeysPressed[e.key] = true;
-    });
-
-    //Deletes all currently unpressed keys from the KeysPressed object.
-    document.addEventListener('keyup', e => {
-        delete KeysPressed[e.key];
-    });
-
-    //Prevents perpetual motion.
-    blockie.dx = 0;
-    blockie.dy = 0;
-
     //xInput and yInput are both used to determine the angle that Blockie is moving in.
-    let xInput = 0;
-    let yInput = 0;
+    xInput = 0;
+    yInput = 0;
 
     //Each key changes the angle of Blockie's movement.
     if (KeysPressed['d']) {
         xInput += 1;
-    }
+    };
 
     if (KeysPressed['a']) {
         xInput -= 1;
-    }
+    };
 
     if (KeysPressed['s']) {
         yInput += 1;
-    }
+    };
 
     if (KeysPressed['w']) {
         yInput -= 1;
-    }
+    };
 
     //Finds the angle that Blockie is moving based on the inputs.
     let angleBlockieMoving = calculateAngleRadians(xInput, yInput);
 
-    //Moves Blockie in the direction of the input, and the trigonometry prevents faster diagonal movement by making 
-    //the desired speed the hypotenuse and finding the x and y components from that.
+    //Stores the desired new location for Blockie by adding blockieSpeed in the input direction and adding that
+    //to Blockie's current location. This variable is then used to prevent Blockie from moving to certain places.
     if (xInput !== 0 || yInput !== 0) {
-        blockie.dx = Math.cos(angleBlockieMoving) * blockieSpeed;
-        blockie.dy = Math.sin(angleBlockieMoving) * blockieSpeed;
-    }
+        blockie.testXLocation = blockie.x + Math.cos(angleBlockieMoving) * blockieSpeed;
+        blockie.testYLocation = blockie.y + Math.sin(angleBlockieMoving) * blockieSpeed;
+    };
 
-    //Updates Blockie's location by adding directional changes to his current location.
-    blockie.x += blockie.dx;
-    blockie.y += blockie.dy;
+    //Updates Blockie's location if it is not off of the canvas. This is divided into x and y components so that
+    //Blockie can move diagonally against the canvas border.
+    if (!(blockie.testXLocation <= 0 || (blockie.testXLocation + blockie.sideLength) >= canvas.width)) {
+        blockie.x = blockie.testXLocation;
+    };
+
+    if (!(blockie.testYLocation <= 0 || (blockie.testYLocation + blockie.sideLength) >= canvas.height)) {
+        blockie.y = blockie.testYLocation;
+    };
 
     //Draws Blockie.
     context.fillStyle = 'red';
